9259ac7f94c5cf16591f705de0bd5a7165d9a02c
Vaishnav253 <vaishnav.nalladath2020@vitstudent.ac.in>
2024-10-29 23:20:01 +0530

    Add files via upload

---
+++ main/java/com/example/tokimekitip/util/Util.kt
@@ -0,0 +1,22 @@
+package com.example.tokimekitip.util
+
+fun calculateTotalTip(totalBill: Double,
+                      tipPercentage: Int): Double {
+    return if (totalBill > 1 &&
+        totalBill.toString().isNotEmpty()) {
+        (totalBill * tipPercentage) / 100
+    } else {
+        0.0
+    }
+}
+
+fun calculateTotalPerPerson(
+    totalBill: Double,
+    splitBy: Int,
+    tipPercentage: Int): Double {
+
+    val Bill = calculateTotalTip(totalBill = totalBill,
+                                 tipPercentage = tipPercentage) + totalBill
+
+    return (Bill / splitBy)
+}
